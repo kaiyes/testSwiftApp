ae46ae667d30debaff1c949f0ce734b63f2233dc
Kaiyes Ansary <kaiyes.ansary@gmail.com>
2019-10-24 10:52:33 +0600

    added basic ui

--- testRay/ContentView.swift
+++ testRay/ContentView.swift
@@ -1,21 +1,52 @@
-//
-//  ContentView.swift
-//  testRay
-//
-//  Created by kaiyes Ansary on 19/10/2019.
-//  Copyright © 2019 kaiyes Ansary. All rights reserved.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State var isOpen: Bool = false
+    @State var popOpener: Bool = false
+
     var body: some View {
-        Text("Hello World")
+        VStack {
+            HStack {
+                Text("Click It")
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.orange)
+                Text("100")
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.orange)
+            }
+
+            HStack {
+                Text("1")
+                Slider(value: .constant(10))
+                Text("100")
+            }
+
+                                            Button(action: {
+                                                print("yet")
+                                                self.isOpen = true
+                                            }) { Text("hit me")}
+            // scores
+            HStack {
+                Button(action: {
+                    print("button start over")
+                }) { Text("Start Over") }
+                Spacer()
+                Text("Score:")
+                Text("9999")
+                Spacer()
+                Text("Round:")
+                Text("999")
+                Spacer()
+                Button(action: {
+                    print("button info")
+                }) { Text("info") }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: 896, height: 414))
     }
 }
